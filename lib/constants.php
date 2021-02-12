98b84be1366254bad2b8b4d3db914f295ccc6717
Tasmia Rahman <tasmiata@gmail.com>
2021-02-12 15:58:41 +0200

    update

---
+++ lib/constants.php
@@ -0,0 +1,13 @@
+<?php
+define('WWW_ROOT', dirname(dirname(__FILE__)));
+
+$directory = basename(WWW_ROOT);
+$url = explode($directory, $_SERVER['REQUEST_URI']);
+
+if (count($url) == 1) {
+	define('WEBROOT', DIRECTORY_SEPARATOR);
+} else{
+	define('WEBROOT',  $url[0] . $directory . DIRECTORY_SEPARATOR);
+}
+
+define('IMAGES', WWW_ROOT . DIRECTORY_SEPARATOR . 'img');
