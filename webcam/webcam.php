07c79edc2d07bd68f2fba9a40f0c32662ef921bf
Tasmia Rahman <tasmiata@gmail.com>
2021-02-18 21:49:28 +0200

    camera working

--- webcam/webcam.php
+++ webcam/webcam.php
@@ -1,25 +1,28 @@
 <?php
-	// echo 'Hello Tasmia';
-	include '../config/db_connection.php';
-	include '../lib/includes.php';
 
-	session_start();
-	$user_id = $_SESSION['user']['user_id'];
+include '../config/db_connection.php';
+include '../lib/includes.php';
 
-	function imagecopymerge_alpha($dst_im, $src_im, $dst_x, $dst_y, $src_x, $src_y, $src_w, $src_h, $pct)
-		{
-			// function patch for respecting alpha work find on http://php.net/manual/en/function.imagecopymerge.php
-			$cut = imagecreatetruecolor($src_w, $src_h);
-			imagecopy($cut, $dst_im, 0, 0, $dst_x, $dst_y, $src_w, $src_h);
-			imagecopy($cut, $src_im, 0, 0, $src_x, $src_y, $src_w, $src_h);
-			imagecopymerge($dst_im, $cut, $dst_x, $dst_y, 0, 0, $src_w, $src_h, $pct);
-		}
+session_start();
 
+$user_id = $_SESSION['user']['user_id'];
 
+function imagecopymerge_alpha($dst_im, $src_im, $dst_x, $dst_y, $src_x, $src_y, $src_w, $src_h, $pct)
+{
+	// function patch for respecting alpha work find on http://php.net/manual/en/function.imagecopymerge.php
+	$cut = imagecreatetruecolor($src_w, $src_h);
+	imagecopy($cut, $dst_im, 0, 0, $dst_x, $dst_y, $src_w, $src_h);
+	imagecopy($cut, $src_im, 0, 0, $src_x, $src_y, $src_w, $src_h);
+	imagecopymerge($dst_im, $cut, $dst_x, $dst_y, 0, 0, $src_w, $src_h, $pct);
+}
 
+echo "Hello BUG?????";
+
+// 2nd Part
 	if (isset($_POST['cpt_1']) && $_POST['cpt_1'] != "" && isset($_POST['alpha'])) {
 		$username = $_SESSION['user']['username'];
 
+		echo "Hello BUG?????";
 		// get the content of the captured image from the webcam put it in a tmp img
 		list($type, $data) = explode(';', $_POST['cpt_1']);
 		list(, $data) = explode(',', $data);
@@ -43,40 +46,87 @@
 
 		$like_count = 0;
 		$sql = "INSERT INTO images (username, user_id, image, like_count) VALUES ('$username', $user_id, '$image_name', '$like_count')";
-		$stmt= $con->prepare($sql);
+		$stmt= $pdo->prepare($sql);
 		$stmt->execute();
 		header('Location: '.$_SERVER['PHP_SELF']);
 		die;
 	}
 	
-		//TODO Need to make sure the picture will pop up to the welcome page after click submit
-		//TODO: Need to connect with DATABASE
 
 
-?>
+if (isset($_FILES['image']) && isset($_POST['alpha'])) {
+	$image = $_FILES['image'];
+	$extension = pathinfo($image['name'], PATHINFO_EXTENSION);
 
+	if (in_array($extension, array('jpg', 'png'))) {
+		$username = $_SESSION['user']['username'];
+		$num = rand(10, 10000);
+		$image_name = $num . '_' . $username . '.' . $extension;
+		move_uploaded_file($image['tmp_name'], IMAGES . '/' . $image_name);
 
-<!-- 
-if ($_SESSION['user'] != NULL) { 
+		if ($extension == 'jpg')
+			$im = imagecreatefromjpeg(IMAGES . '/' . $image_name);
+		else if ($extension == 'png')
+			$im = imagecreatefrompng(IMAGES . '/' . $image_name);
 
+		$alpha = imagecreatefrompng(IMAGES . '/alpha/' . $_POST['alpha'] . '.png');
+		// imagecopymerge_alpha($dst_im, $src_im, $dst_x, $dst_y, $src_x, $src_y, $src_w, $src_h, $pct)
+		imagecopymerge_alpha($im, $alpha, 0, 0, 0, 0, imagesx($alpha), imagesy($alpha), 100);
 
-	
+		imagepng($im,  IMAGES . '/' . $image_name);
+		imagedestroy($im);
+		$image = $image_name;
+		$like_count = 0;
 
+		// sql request
+		$sql = "INSERT INTO images (username, user_id, image, like_count) VALUES ('$username', $user_id, '$image', '$like_count')";
+		$stmt= $pdo->prepare($sql);
+		$stmt->execute();
 
-
+		header('Location: '.$_SERVER['PHP_SELF']);
+		die;
+	}
 }
 
-else{
-	header('Location: ../index.php');
-} -->
+
+// Delete Button will show up once the submit picture will appear on the display
+// Is delete button appear then this code will help out to delete the photo user's want
+if (isset($_GET['delete'])) {
+
+	$id = $_GET['delete'];
+	$results = $pdo->prepare("SELECT * FROM images WHERE id=$id");
+	$results->execute();
+	$images = $results->fetchAll();
+	foreach ($images as $image) {
+		unlink("../img/" . $image['image']);
+		if ($image['username'] == $_SESSION['user']['username']) {
+			$sql = "DELETE FROM images WHERE id=$id";
+			$stmt= $pdo->prepare($sql);
+			$stmt->execute();
+			header('Location: webcam.php');
+			die();
+		}
+	}
+}
+
+$results = $pdo->prepare("SELECT * FROM images ORDER BY id DESC");
+$results->execute();
+$images = $results->fetchAll();
+
+
+// HTML SECTION
+
+if ($_SESSION['user'] != NULL) { ?>
 
 <html>
 	<head>
 		<title>Camagru</title>
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
 		<style type="text/css">
 			#results { padding:20px; border:1px solid; background:#ccc; }
 		</style>
+
 		<link rel="stylesheet" href="main.css"/>
 		<!-- <link rel="stylesheet" href="webcam.css"/> -->
 		<script src="capture.js"></script>
@@ -92,7 +142,7 @@ else{
 							<input type="hidden" name="cpt_1" id="cpt_1" class="image-tag">
 							<div class="camera">
 								<video id="video">Video stream not available.</video>
-								<button id="startbutton" type="button">Take photo</button> 
+								<button id="startbutton" type="button">Take photo</button>
 							</div>
 						</div>
 
@@ -102,9 +152,10 @@ else{
 								<img id="photo" alt="The screen capture will appear in this box.">
 							</div>
 
+<!-- <h1>Hello BUG</h1> -->
+
 						</div>
 					</div>
-
 
 					<div class="row" style="margin-top: 1vw">
 						<div class="col col-md-6 text-center">
@@ -116,19 +167,19 @@ else{
 						<div class="col-md-12 text-center">
 							<input class="checkbox-tools" type="hidden" name="alpha" value="blank" id="tool-0" checked>
 							<!-- 1st emoji -->
-							<input class="checkbox-tools" type="radio" name="alpha" value="emoji1" id="tool-1">
+							<input class="checkbox-tools" type="radio" name="alpha" value="alphatest1" id="tool-1">
 							<label class="for-checkbox-tools" for="tool-1">
-								<img style="width: 118px; margin-left: -15px;" src="<?php echo WEBROOT; ?>img/emoji/emoji1.png">
+								<img style="width: 118px; margin-left: -15px;" src="<?php echo WEBROOT; ?>img/alpha/alphatest1.png">
 							</label>
 							<!-- 2nd emoji -->
-							<input class="checkbox-tools" type="radio" name="alpha" value="emoji2" id="tool-2">
+							<input class="checkbox-tools" type="radio" name="alpha" value="alphatest2" id="tool-2">
 							<label class="for-checkbox-tools" for="tool-2">
-								<img style="width: 50px; margin-left: -2px;" src="<?php echo WEBROOT; ?>img/emoji/emoji2.png">
+								<img style="width: 50px; margin-left: -2px;" src="<?php echo WEBROOT; ?>img/alpha/alphatest2.png">
 							</label>
 							<!-- 3rd emoji -->
-							<input class="checkbox-tools" type="radio" name="alpha" value="emoji3" id="tool-3">
+							<input class="checkbox-tools" type="radio" name="alpha" value="alphatest3" id="tool-3">
 							<label class="for-checkbox-tools" for="tool-3">
-								<img style="width: 100px;margin-left: 0px;" src="<?php echo WEBROOT; ?>img/emoji/emoji3.png">
+								<img style="width: 100px;margin-left: 0px;" src="<?php echo WEBROOT; ?>img/alpha/alphatest3.png">
 							</label>
 
 						</div>
@@ -166,3 +217,12 @@ else{
 		<?php include '../includes//footer.php'; ?>
 	</body>
  </html>
+ <?php
+}
+
+else{
+	header('Location: ../index.php');
+}
+
+// Final closing TAG
+?>
