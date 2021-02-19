94fc8d77cfa391ab842896b953e2c40b1d367a66
Tasmia Rahman <tasmiata@gmail.com>
2021-02-19 22:12:13 +0200

    bug fix

--- webcam/capture.js
+++ webcam/capture.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
 	// The width and height of the captured photo. We will set the
 	// width to the value defined here, but the height will be
 	// calculated based on the aspect ratio of the input stream.
@@ -20,58 +20,58 @@
 	var startbutton = null;
 
 	function startup() {
-	  video = document.getElementById('video');
-	  canvas = document.getElementById('canvas');
-	  photo = document.getElementById('photo');
-	  startbutton = document.getElementById('startbutton');
+		video = document.getElementById('video');
+		canvas = document.getElementById('canvas');
+		photo = document.getElementById('photo');
+		startbutton = document.getElementById('startbutton');
 
-	  navigator.mediaDevices.getUserMedia({video: true, audio: false})
-	  .then(function(stream) {
-		video.srcObject = stream;
-		video.play();
-	  })
-	  .catch(function(err) {
-		console.log("An error occurred: " + err);
-	  });
+		navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+			.then(function (stream) {
+				video.srcObject = stream;
+				video.play();
+			})
+			.catch(function (err) {
+				console.log("An error occurred: " + err);
+			});
 
-	  video.addEventListener('canplay', function(ev){
-		if (!streaming) {
-		  height = video.videoHeight / (video.videoWidth/width);
+		video.addEventListener('canplay', function (ev) {
+			if (!streaming) {
+				height = video.videoHeight / (video.videoWidth / width);
 
-		  // Firefox currently has a bug where the height can't be read from
-		  // the video, so we will make assumptions if this happens.
-		
-		  /*
-		  if (isNaN(height)) {
-			height = width / (4/3);
-		  } */
+				// Firefox currently has a bug where the height can't be read from
+				// the video, so we will make assumptions if this happens.
 
-		  video.setAttribute('width', width);
-		  video.setAttribute('height', height);
-		  canvas.setAttribute('width', width);
-		  canvas.setAttribute('height', height);
-		  streaming = true;
-		}
-	  }, false);
+				/*
+				if (isNaN(height)) {
+				  height = width / (4/3);
+				} */
 
-	  startbutton.addEventListener('click', function(ev){
-		takepicture();
-		ev.preventDefault();
-	  }, false);
-	  
-	  clearphoto();
+				video.setAttribute('width', width);
+				video.setAttribute('height', height);
+				canvas.setAttribute('width', width);
+				canvas.setAttribute('height', height);
+				streaming = true;
+			}
+		}, false);
+
+		startbutton.addEventListener('click', function (ev) {
+			takepicture();
+			ev.preventDefault();
+		}, false);
+
+		clearphoto();
 	}
 
 	// Fill the photo with an indication that none has been
 	// captured.
 
 	function clearphoto() {
-	  var context = canvas.getContext('2d');
-	  context.fillStyle = "#AAA";
-	  context.fillRect(0, 0, canvas.width, canvas.height);
-  
-	  var data = canvas.toDataURL('image/png');
-	  photo.setAttribute('src', data);
+		var context = canvas.getContext('2d');
+		context.fillStyle = "#AAA";
+		context.fillRect(0, 0, canvas.width, canvas.height);
+
+		var data = canvas.toDataURL('image/png');
+		photo.setAttribute('src', data);
 	}
 
 	// Capture a photo by fetching the current contents of the video
@@ -81,24 +81,25 @@
 	// other changes before drawing it.
 
 	function takepicture() {
-	  var context = canvas.getContext('2d');
-	  if (width && height) {
-		canvas.width = width;
-		canvas.height = height;
-		context.drawImage(video, 0, 0, width, height);
-	  
-		var data = canvas.toDataURL('image/png');
-		// console.log(height);
-		photo.setAttribute('src', data);
+		var context = canvas.getContext('2d');
+		if (width && height) {
+			canvas.width = width;
+			canvas.height = height;
+			context.drawImage(video, 0, 0, width, height);
 
-		// new addition by myself to get the id=cpt_1 data from DOM
-		document.getElementById('cpt_1').value = data;
-	  } else {
-		clearphoto();
-	  }
+			var data = canvas.toDataURL('image/png');
+			// console.log(height);
+			photo.setAttribute('src', data);
+
+			// new addition by myself to get the id=cpt_1 data from DOM
+			document.getElementById('cpt_1').value = data;
+		}
+		else {
+			clearphoto();
+		}
 	}
 
 	// Set up our event listener to run the startup process
 	// once loading is complete.
 	window.addEventListener('load', startup, false);
-  })();
+})();
