efafad9e390ea7b66107dec53b6db3d4a07b3f20
Tasmia Rahman <tasmiata@gmail.com>
2021-03-08 14:03:56 +0200

    final version of index.php

--- index.php
+++ index.php
@@ -1,60 +1,65 @@
 <?php
-// Start the session
 session_start();
 
-// Add all the include files
 include 'config/setup.php';
 include 'config/db_connection.php';
 
-// Set time zone
 date_default_timezone_set('Europe/Helsinki');
 
-// Prepare the SQL query to display all the posted image
-$results = $pdo->prepare("SELECT users.username, images.image FROM users JOIN images ON images.user_id = users.user_id ORDER BY id DESC");
-$results->execute();
-$images = $results->fetchAll();
+$sqlQuery = $pdo->prepare("SELECT users.username, images.image FROM users JOIN images ON images.user_id = users.user_id ORDER BY id DESC");
+$sqlQuery->execute();
+$images = $sqlQuery->fetchAll();
 ?>
 
 <!doctype html>
 <html lang="en">
-	<head>
-	<!-- Bootstrap & CSS Link -->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-	<link rel="stylesheet" href="../gallery/gallery.css">
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 	<title>Index Page</title>
-	</head>
+	<style>
+		img {
+			max-width: 100%;
+			max-height: 100%;
+		}
+	</style>
+</head>
 
-	<body>
-	<?php require 'navbar.php' ?>
+<body>
+	<?php include 'navbar.php'; ?>
+			<div class="row" style="margin: 0.3vw; margin-top: 6vw;">
+				<?php foreach($images as $image)
+				{ ?>
+					<div class="col-lg-3 col-xs-6 col-md-3 text-center" style="padding: 10px">
+						<div class="card h-100 bg-dark text-white image-card">
+							<!-- card header -->
+							<div class="card-header text-center">
+								<p>Post Owner: <a href=""><?php echo $image['username'];?></a></p>
+							</div>
+							<!-- card body -->
+							<div style="margin: .2vw;">
+								<?php if ($_SESSION['user'] != NULL) { ?>
+									<a href="../controller/likes.php?imageid=<?php echo $image['id'];?>"><img class="img-responsive" src="<?php echo '../img/'. $image['image']; ?>" margin="auto" /></a>
+									</br>
+								<?php }
+								else { ?> 
+									<img src="<?php echo 'img/'. $image['image']; ?>" margin="auto" />
+								<?php } ?> 
+							</div>
+							<!-- card footer -->
+							<div class="card-footer text-center">
+								<button onclick="return confirm('Please LOGIN to Like & Comment')" class="btn btn-secondary">Like & Comment</button>
+							</div>
+						</div>
 
-	<div class="row" style="margin: .5vw">
-
-		<?php foreach($images as $image){ ?>
-			<!-- <div class="col-lg-3 col-xs-6 col-md-3 text-center" > -->
-			<div class="col-lg-3 col-xs-10 col-md-4">
-				<div class="card h-100 bg-secondary text-white image-card">
-					<div class="card-header text-center">
-						<p class="card-title">Post Owner: <a href="#" class="text-white "><?php echo $image['username'];?></a></p>
 					</div>
-
-					<div class="card-img-bottom">
-						<?php if ($_SESSION['user'] != NULL) { ?>
-							<a href="like.php?imageid=<?php echo $image['id'];?>">
-								<img src="<?php echo '../img/'. $image['image']; ?>"/>
-							</a>
-							</br>
-						<?php }
-
-						else { ?>
-							<img src="<?php echo 'img/'. $image['image']; ?>"/>
-						<?php } ?> 
-					</div>
-				
-				</div>
+				<?php } ?>
 			</div>
-		<?php } ?>
-	</div>
-	</body>
-
-	<?php require 'includes/footer.php' ?>
+			<?php include 'includes/footer.php'; ?>
+		</div>
+</body>
 </html>
