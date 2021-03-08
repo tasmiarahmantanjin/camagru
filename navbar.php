650c9c3d57855a9c9a0c6bd71988900aedc1b8e6
Tasmia Rahman <tasmiata@gmail.com>
2021-03-08 14:04:13 +0200

    final version of navbar

--- navbar.php
+++ navbar.php
@@ -1,36 +1,19 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-
-<!-- <nav class="navbar navbar-dark bg-primary"> -->
-	<!-- <a class="navbar-brand" href="../index.php">Camagru</a> -->
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
 	<a class="navbar-brand" href="index.php">Camagru</a>
-	<!-- <a class="navbar-brand" href="../index.php">Camagru</a> -->
 
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-
-	<span class="navbar-toggler-icon"></span>
+		<span class="navbar-toggler-icon"></span>
 	</button>
 
 	<div class="collapse navbar-collapse" id="navbarSupportedContent">
-
 	<ul class="navbar-nav mr-auto">
 		<li class="nav-item">
-			<!-- <a class="nav-link" href="login.php">Sign in</a> -->
 			<a class="nav-link" href="user/login.php">Sign in</a>
 		</li>
 
 		<li class="nav-item">
-			<!-- <a class="nav-link" href="register.php">Sign up</a> -->
 			<a class="nav-link" href="user/register.php">Sign up</a>
 		</li>
-
-		<!-- <li class="nav-item">
-			<a class="nav-link" href="logout.php">Sign out</a>
-		</li> -->
 	</ul>
-
-	<form class="form-inline my-2 my-lg-0">
-		<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-		<button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
-	</form>
 	</div>
 </nav>
